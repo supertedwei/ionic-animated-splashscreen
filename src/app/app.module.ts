8ce0538f5693a93aa9473112229a42a667a803a0
Ted Wei <jdwei@hotmail.com>
2017-05-12 10:56:30 +0800

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { Splash } from '../pages/splash/splash';
+ 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    Splash
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    Splash
   ],
   providers: [
     StatusBar,
--- src/pages/splash/splash.ts
+++ src/pages/splash/splash.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SplashPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-@IonicPage()
+import { ViewController } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+ 
 @Component({
   selector: 'page-splash',
   templateUrl: 'splash.html',
 })
-export class SplashPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class Splash {
+ 
+  constructor(public viewCtrl: ViewController, public splashScreen: SplashScreen) {
+ 
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SplashPage');
+ 
+  ionViewDidEnter() {
+ 
+    this.splashScreen.hide();
+ 
+    setTimeout(() => {
+      this.viewCtrl.dismiss();
+    }, 4000);
+ 
   }
-
+ 
 }
